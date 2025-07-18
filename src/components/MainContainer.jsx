c4e3b24009202f6bd7f1aea45d1aa66a7666a611
Saddam Hussein <work.saddam786@gmail.com>
2025-07-18 11:16:31 +0530

    add shimmer ui

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,11 +1,12 @@
 import { useSelector } from "react-redux";
 import { VideoTitle } from "./VideoTitle";
 import { VideoBackground } from "./VideoBackground";
+import Shimmer from "./Shimmer";
 
 export const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
   // console.log("🎬 From store.movies.nowPlayingMovies:", movies);
-  if (!movies) return;
+  if (!movies) return <Shimmer/>;
 
   const mainMovie = movies[0];
 //   console.log(mainMovie);
